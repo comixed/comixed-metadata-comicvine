bb645e555ab42eb482072f4a8461cb342e21f1cc
Darryl L. Pierce <mcpierce@gmail.com>
2024-07-31 13:51:58 -0400

    Added support for the www.comicvine.com web address [#22]

--- src/test/java/org/comixedproject/metadata/comicvine/adaptors/ComicVineMetadataAdaptorProviderTest.java
+++ src/test/java/org/comixedproject/metadata/comicvine/adaptors/ComicVineMetadataAdaptorProviderTest.java
@@ -9,10 +9,12 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ComicVineMetadataAdaptorProviderTest {
-  private static final String TEST_GOOD_ADDRESS =
+  private static final String TEST_GOOD_ADDRESS_1 =
       "https://comicvine.gamespot.com/action-comics-futures-end-1-crossroads/4000-463937/";
+  private static final String TEST_GOOD_ADDRESS_2 =
+      "https://www.comicvine.com/action-comics-futures-end-1-crossroads/4000-463937/";
   private static final String TEST_BAD_ADDRESS =
-      "https://notcomicvine.gamespot.com/action-comics-futures-end-1-crossroads/4000-463937";;
+      "https://notcomicvine.gamespot.com/action-comics-futures-end-1-crossroads/4000-463937";
 
   @InjectMocks private ComicVineMetadataAdaptorProvider provider;
 
@@ -22,7 +24,22 @@ public class ComicVineMetadataAdaptorProviderTest {
   }
 
   @Test
-  public void testSupportedReference() {
-    assertTrue(provider.supportedReference(TEST_GOOD_ADDRESS));
+  public void testSupportedReferenceOldHttpComicVineDomain() {
+    assertTrue(provider.supportedReference(TEST_GOOD_ADDRESS_2.replace("https", "http")));
+  }
+
+  @Test
+  public void testSupportedReferenceOldComicVineDomain() {
+    assertTrue(provider.supportedReference(TEST_GOOD_ADDRESS_2));
+  }
+
+  @Test
+  public void testSupportedReferenceCurrentHttpComicVineDomain() {
+    assertTrue(provider.supportedReference(TEST_GOOD_ADDRESS_1.replace("https", "http")));
+  }
+
+  @Test
+  public void testSupportedReferenceCurrentComicVineDomain() {
+    assertTrue(provider.supportedReference(TEST_GOOD_ADDRESS_1));
   }
 }
